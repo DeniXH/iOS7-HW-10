93f368905ca4c8a53605c96fd54873eceeedfc27
Denis <den88000@gmail.com>
2022-07-28 22:01:34 +0300

    delete spaces and commit

--- iOS7-HW-10/ViewController.swift
+++ iOS7-HW-10/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  iOS7-HW-10
-//
-//  Created by Денис Холодков on 28.07.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
