3abbf02dd172b3eb70b1b4f9dd0e011cec18620f
Denis <den88000@gmail.com>
2022-07-28 21:48:04 +0300

    Initial Commit

---
+++ iOS7-HW-10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS7-HW-10
+//
+//  Created by Денис Холодков on 28.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
